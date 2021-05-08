89b625b25bd1e51b27f81831ffe89d58dbdcbd99
GermanSanchezb <g.gsanchezbertola@gmail.com>
2021-05-08 14:56:08 -0300

    timeline

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <Reserva/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Reserva from './components/reserva.vue';
+module.exports = {
+  entry:'./components/timeline/timeline.html'
+}
+
+export default {
+  name: 'App',
+
+  components: {
+    Reserva,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/reserva.vue
@@ -0,0 +1,70 @@
+<template>
+   <div>
+    <div id="index">
+      <v-app>
+        <v-main>
+          <v-container class="grey lighten-5 mb-6">
+              <h3>Reservar</h3>
+              <v-row no-gutters style="height: 150px;" mb="6">
+                  <v-col cols="12" sm="4">
+                      <v-text-field
+                      label="Nombre"
+                      required
+                      outlined>
+                      </v-text-field>
+                  </v-col>
+
+                  <v-col cols="6" sm="2">
+                      <v-text-field
+                      label="Fecha"
+                      value=""
+                      type="date"
+                      append-icon="mdi-calendar"
+                      outlined
+                      >
+                      </v-text-field>
+                  </v-col> 
+
+                  <v-col cols="6" sm="2">
+                      <v-text-field
+                      label="Desde"
+                      value=""
+                      type="time"
+                      append-icon="mdi-clock"
+                      outlined
+                      >
+                      </v-text-field>
+                  </v-col>
+
+                  <v-col cols="6" sm="2">
+                      <v-text-field
+                      label="Hasta"
+                      value=""
+                      type="time"
+                      append-icon="mdi-clock"
+                      outlined
+                      >
+                      </v-text-field>
+                  </v-col>
+                  <v-col cols= "1"></v-col>
+                  <v-col cols="1">
+                      <v-btn
+                      depressed
+                      elevation ="1"
+                      color="success"
+                      fab>
+                        <v-icon>mdi-check</v-icon>
+                      </v-btn>
+                  </v-col>
+              </v-row>
+          </v-container>
+        </v-main>
+      </v-app>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'reserva',
+  }
+</script>
